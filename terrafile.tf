7be66b853ca35c1b85396dbb3d385440f2596298
André Almeida <alfalmeida@hotmail.com>
2025-02-18 15:54:30 -0300

    Update terrafile.tf

--- terrafile.tf
+++ terrafile.tf
@@ -2,7 +2,7 @@ data "external" "decrypted_secret" {
   program = ["sops", "--decrypt", "--output-type", "json", "${path.module}/secret.json.enc"]
 }
 
-module "pay2free_secret" {
+module "my_secret" {
   source      = "./modules/aws-secrets-manager-secrets"
   region      = "MyRegion"  
   secret_name = "MySecretName"
